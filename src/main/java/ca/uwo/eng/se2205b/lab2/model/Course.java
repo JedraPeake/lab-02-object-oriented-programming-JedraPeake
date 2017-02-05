3f75a1f7b3ed1d8532666c395710b18fc9c886a3
JedraPeake <jpeake2@uwo.ca>
2017-02-05 18:34:17 -0500

    Q1 initial commit

--- src/main/java/ca/uwo/eng/se2205b/lab2/model/Course.java
+++ src/main/java/ca/uwo/eng/se2205b/lab2/model/Course.java
@@ -1,6 +1,7 @@
 package ca.uwo.eng.se2205b.lab2.model;
 
 import javax.annotation.Nonnull;
+import java.util.List;
 
 /**
  * Describes a course offering at a University
@@ -11,7 +12,7 @@ public interface Course {
      * Get the unique course code for the Course.
      * @return Non-{@code null} or empty code for the course.
      */
-    String getCourseCode();
+    public String getCourseCode();
 
     /**
      * Set the name of the course
@@ -20,51 +21,51 @@ public interface Course {
      * @throws NullPointerException if {@code name} is {@code null}.
      * @throws IllegalArgumentException if {@code name} is empty.
      */
-    void setName(@Nonnull String name);
+    public void setName(@Nonnull String name);
 
     /**
      * Get the current name of a course
      * @return Current name
      */
-    String getName();
+    public String getName();
 
     /**
      * Change the {@link Department} for a {@code Course}
      * @param newDepartment New department a course resides in.
      */
-    void setDepartment(Department newDepartment);
+    public void setDepartment(Department newDepartment);
 
     /**
      * Get the current {@link Department}
      * @return Possibly {@code null} department.
      */
-    Department getDepartment();
+    public Department getDepartment();
 
     /**
      * Get the maximum occupancy of the course
      * @return Maximum number of students in the course.
      */
-    int getMaximumOccupancy();
+    public int getMaximumOccupancy();
 
-//    /**
-//     * Enroll a student in the course.
-//     * @param student Non-{@code null} student to add.
-//     *
-//     * @throws IllegalStateException if there is no room.
-//     */
-//    void enrollStudent(@Nonnull Student student);
-//
-//    /**
-//     * Remove a {@link Student} from a {@code Course}
-//     * @param student Removed student
-//     * @return The Student instance, if they were removed, otherwise {@code null}.
-//     */
-//    Student removeStudent(@Nonnull Student student);
-//
-//    /**
-//     * Get all of the currently enrolled students
-//     * @return Non-{@code null} {@code List} of {@link Student}s, it may be empty.
-//     */
-//    List<Student> getEnrolledStudents();
+    /**
+     * Enroll a student in the course.
+     * @param student Non-{@code null} student to add.
+     *
+     * @throws IllegalStateException if there is no room.
+     */
+    public void enrollStudent(@Nonnull Student student);
+
+    /**
+     * Remove a {@link Student} from a {@code Course}
+     * @param student Removed student
+     * @return The Student instance, if they were removed, otherwise {@code null}.
+     */
+    public Student removeStudent(@Nonnull Student student);
+
+    /**
+     * Get all of the currently enrolled students
+     * @return Non-{@code null} {@code List} of {@link Student}s, it may be empty.
+     */
+    public List<Student> getEnrolledStudents();
 
 }
--- src/main/java/ca/uwo/eng/se2205b/lab2/model/Department.java
+++ src/main/java/ca/uwo/eng/se2205b/lab2/model/Department.java
@@ -12,7 +12,7 @@ public interface Department {
      * Get the unique name of the Department
      * @return Non-{@code null} name
      */
-    String getName();
+    public String getName();
 
     /**
      * Return the unique name of the department.
@@ -20,44 +20,44 @@ public interface Department {
      *
      * @throws IllegalArgumentException if {@code name} is empty
      */
-    void setName(@Nonnull String name);
+    public void setName(@Nonnull String name);
 
-//    /**
-//     * Enroll a student in the Department.
-//     * @param student Non-{@code null} student to add.
-//     */
-//    void enrollStudent(@Nonnull Student student);
-//
-//    /**
-//     * Remove a {@link Student} from a {@code Department}
-//     * @param student Removed student
-//     * @return The Student instance, if they were removed, otherwise {@code null}.
-//     */
-//    Student removeStudent(@Nonnull Student student);
-//
-//    /**
-//     * Get all of the currently enrolled students
-//     * @return Non-{@code null} {@code List} of {@link Student}s, it may be empty.
-//     */
-//    List<Student> getEnrolledStudents();
+    /**
+     * Enroll a student in the Department.
+     * @param student Non-{@code null} student to add.
+     */
+    public void enrollStudent(@Nonnull Student student);
+
+    /**
+     * Remove a {@link Student} from a {@code Department}
+     * @param student Removed student
+     * @return The Student instance, if they were removed, otherwise {@code null}.
+     */
+    public Student removeStudent(@Nonnull Student student);
+
+    /**
+     * Get all of the currently enrolled students
+     * @return Non-{@code null} {@code List} of {@link Student}s, it may be empty.
+     */
+    public List<Student> getEnrolledStudents();
 
     /**
      * Adds a course to a department
      * @param course Course to add
      */
-    void addCourse(@Nonnull Course course);
+    public void addCourse(@Nonnull Course course);
 
     /**
      * Removes a course from the {@code Department}
      * @param course Course to remove
      * @return Instance removed, {@code null} if none are removed
      */
-    Course removeCourse(@Nonnull Course course);
+    public Course removeCourse(@Nonnull Course course);
 
     /**
      * Get all of the courses in the Department
      * @return List of all courses, may be empty, but never {@code null}
      */
-    List<Course> getCourses();
+    public List<Course> getCourses();
 
 }
